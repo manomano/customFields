1d379d1ee36170eeb491e036d4028f15754d98ba
manomano <manana.japaridze@gmail.com>
2019-10-17 22:05:04 +0400

    drawing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import DateField from "./customDateField";
-import Autocomplete from "./Autocomplete";
+//import DateField from "./customDateField";
+//import Autocomplete from "./Autocomplete";
 
 
 import "./styles.css";
@@ -11,13 +11,13 @@ function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <DateField date="month" value={"2019-01"} />
-      <div>
-        <Autocomplete
-          parname="name_startsWith"
-          url="https://secure.geonames.org/searchJSON?lang=eng&style=full&type=json&maxRows=10&username=georchestra"
-        />
-      </div>
+      {/*<DateField date="month" value={"2019-01"} />*/}
+      {/*<div>*/}
+        {/*<Autocomplete*/}
+          {/*parname="name_startsWith"*/}
+          {/*url="https://secure.geonames.org/searchJSON?lang=eng&style=full&type=json&maxRows=10&username=georchestra"*/}
+        {/*/>*/}
+      {/*</div>*/}
         <div>
         <MapContainer/>
         </div>
--- src/mapField.js
+++ src/mapField.js
@@ -11,6 +11,7 @@ import Polygon from 'ol/geom/Polygon';
 import Draw, {createRegularPolygon, createBox} from 'ol/interaction/Draw';
 import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
 import {OSM, Vector as VectorSource} from 'ol/source';
+import {Fill, RegularShape, Stroke, Style} from 'ol/style';
 
 
 
@@ -19,6 +20,26 @@ import {OSM, Vector as VectorSource} from 'ol/source';
 
 
 export default class MapContainer extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            map: null,
+            vectorSource: null,
+            drawInteraction: null,
+            feature: props.feature || null,
+            center: props.center || [4961441.382031006, 5129644.04584318],
+            zoom: props.zoom || 6
+        }
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(){
+        this.state.map.removeInteraction(this.state.drawInteraction);
+        this.state.map.addInteraction(this.state.drawInteraction);
+    }
+
 
     componentDidMount() {
         const raster = new TileLayer({
@@ -40,10 +61,60 @@ export default class MapContainer extends React.Component{
             })
         });
 
+
+        const draw = new Draw({
+            source: source,
+            type: 'Circle',
+            geometryFunction: createBox(),
+            style:  new Style({
+                stroke: new Stroke({
+                    color: 'red',
+                    width: 3
+                }),
+                fill: new Fill({
+                    color: 'rgba(0, 0, 255, 0.1)'
+                })
+
+            }),
+        });
+
+
+        this.setState({
+            map: map,
+            vectorSource: source,
+            drawInteraction: draw
+        });
+
+
+
+        map.on('click', function(event) {
+             if(source.getFeatures().length>1){
+                 source.removeFeature(source.getFeatures()[0]);
+             }
+        });
+
+
+
+
     }
 
     render(){
-        return (<div id="map"></div>)
+        return (<div className="container" style={{border:'1px solid red'}}>
+            <div className="row justify-content-center">
+                <input type="number" id="top" />
+                <button className="btn btn-primary" onClick={this.handleClick}><i className="far fa-edit"></i> draw</button>
+                </div>
+                <div className="row align-middle">
+                    <div className="col-2 justify-content-start align-middle">
+                        <input type="number" style={{width:'100%'}} id="left"/>
+                    </div>
+                    <div id="map" className="col-8"></div>
+                    <div className="col-2 justify-content-end align-middle">
+                        <input type="number" style={{width:'100%'}} id="right"/>
+                    </div>
+                </div>
+                <div className="row justify-content-center"><input type="number" id="bottom"/></div>
+            </div>)
     }
 
 }
